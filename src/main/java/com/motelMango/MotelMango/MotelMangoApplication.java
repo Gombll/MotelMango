346effdd28da23c7d8e25af1b7f1c8e66c80f2d7
ALVARO OCTAVIO VEGA TICLIAHUANCA <U22204373@utp.edu.pe>
2025-05-13 13:55:05 -0500

    first commit

---
+++ src/main/java/com/motelMango/MotelMango/MotelMangoApplication.java
@@ -0,0 +1,13 @@
+package com.motelMango.MotelMango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotelMangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotelMangoApplication.class, args);
+	}
+
+}
